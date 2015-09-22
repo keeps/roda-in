af6561e16f040a294581b17efaf8eedb74a6dfc7
André Pereira <apereira@keep.pt>
2015-09-22 17:51:33 +0100

    Implemented a rudimentary image creation for the description level logos

--- src/main/java/schema/ui/SchemaNode.java
+++ src/main/java/schema/ui/SchemaNode.java
@@ -4,19 +4,21 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import schema.DescriptionObject;
+import schema.ui.DescriptionLevel.DescriptionLevelImageCreator;
 
 /**
  * Created by adrap on 17-09-2015.
  */
 public class SchemaNode extends TreeItem<String> {
-    public static Image folderCollapseImage = new Image(ClassLoader.getSystemResourceAsStream("folder.png"));
     public DescriptionObject dob;
 
     public SchemaNode(DescriptionObject dobject) {
         super(dobject.getTitle());
         dob = dobject;
 
-        this.setGraphic(new ImageView(folderCollapseImage));
+        DescriptionLevelImageCreator dlic = new DescriptionLevelImageCreator(dob.getDescriptionlevel());
+        Image im = dlic.generate();
+        this.setGraphic(new ImageView(im));
 
         for(DescriptionObject obj: dob.getChildren()){
             SchemaNode child = new SchemaNode(obj);
