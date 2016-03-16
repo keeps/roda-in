476e2be3b28db0b0359e0ea860fedfc2d41815f3
André Pereira <apereira@keep.pt>
2016-03-16 16:16:09 +0000

    ResourceResolver now tries to get the import from the internet if the attribute is a URL. Updated some language strings.

--- src/main/java/org/roda/rodain/utils/validation/ResourceResolver.java
+++ src/main/java/org/roda/rodain/utils/validation/ResourceResolver.java
@@ -3,7 +3,10 @@ package org.roda.rodain.utils.validation;
 import org.w3c.dom.ls.LSInput;
 import org.w3c.dom.ls.LSResourceResolver;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author Andre Pereira apereira@keep.pt
@@ -12,9 +15,16 @@ import java.io.InputStream;
 public class ResourceResolver implements LSResourceResolver {
   @Override
   public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
-
-    // the XSD's are expected to be in the root of the classpath
-    InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(systemId);
+    InputStream resourceAsStream = null;
+    try {
+      URL url = new URL(systemId);
+      resourceAsStream = url.openStream();
+    } catch (MalformedURLException e) {
+      // the XSD's are expected to be in the root of the classpath
+      resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(systemId);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return new Input(publicId, systemId, resourceAsStream);
   }
 
